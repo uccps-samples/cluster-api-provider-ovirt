850a421b414a7a3d7684b18563349ecee3b2d39b
Gal-Zaidman <gzaidman@redhat.com>
2021-03-11 10:34:10 +0200

    Change name to providerIDController and remove node Controller logic

--- pkg/cloud/ovirt/controllers/providerIDcontroller/providerIDController.go
+++ pkg/cloud/ovirt/controllers/providerIDcontroller/providerIDController.go
@@ -5,11 +5,9 @@ import (
 	"fmt"
 	"github.com/openshift/cluster-api-provider-ovirt/pkg/cloud/ovirt"
 	common "github.com/openshift/cluster-api-provider-ovirt/pkg/cloud/ovirt/controllers"
-	ovirtsdk "github.com/ovirt/go-ovirt"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/klog/klogr"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -18,16 +16,15 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-var _ reconcile.Reconciler = &providerIDReconciler{}
+var _ reconcile.Reconciler = &providerIDController{}
 
-type providerIDReconciler struct {
+type providerIDController struct {
 	common.BaseController
 	listNodesByFieldFunc func(key, value string) ([]corev1.Node, error)
-	fetchProviderIDFunc  func(string) (string, error)
 }
 
-func (r *providerIDReconciler) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
-	r.Log.Info("Reconciling", "Node", request.NamespacedName)
+func (r *providerIDController) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	r.Log.Info("ProviderIDController, Reconciling", "Node", request.NamespacedName)
 
 	// Fetch the Node instance
 	node := corev1.Node{}
@@ -42,30 +39,11 @@ func (r *providerIDReconciler) Reconcile(ctx context.Context, request reconcile.
 		// Error reading the object - requeue the request.
 		return reconcile.Result{}, fmt.Errorf("error getting node: %v", err)
 	}
-	id, err := r.fetchProviderIDFunc(node.Name)
-	if err != nil {
-		return reconcile.Result{}, fmt.Errorf("failed getting VM from oVirt: %v", err)
-	}
-	if id == "" {
-		// Node doesn't exist in oVirt platform, deleting node object
-		r.Log.Info(
-			"Deleting Node from cluster since it has been removed from the oVirt engine",
-			"node", request.NamespacedName)
-		return deleteNode(ctx, r.Client, &node)
-	}
-	if node.Spec.ProviderID != "" {
-		// Node exist and providerID is set
-		c, err := r.GetConnection(common.NAMESPACE, common.CREDENTIALS_SECRET)
-		vmResponse, err := c.SystemService().VmsService().VmService(id).Get().Send()
+	if node.Spec.ProviderID == "" {
+		id, err := r.fetchOvirtVmID(node.Name)
 		if err != nil {
 			return reconcile.Result{}, fmt.Errorf("failed getting VM from oVirt: %v", err)
 		}
-		if vmResponse.MustVm().MustStatus() == ovirtsdk.VMSTATUS_DOWN {
-			r.Log.Info("Node VM status is Down, requeuing for 1 min",
-				"Node", node.Name, "Vm Status", ovirtsdk.VMSTATUS_DOWN)
-			return reconcile.Result{Requeue: true, RequeueAfter: common.RETRY_INTERVAL_VM_DOWN}, nil
-		}
-	} else {
 		r.Log.Info("spec.ProviderID is empty, fetching from ovirt", "node", request.NamespacedName)
 		node.Spec.ProviderID = ovirt.ProviderIDPrefix + id
 		err = r.Client.Update(ctx, &node)
@@ -76,14 +54,7 @@ func (r *providerIDReconciler) Reconcile(ctx context.Context, request reconcile.
 	return reconcile.Result{}, nil
 }
 
-func deleteNode(ctx context.Context, client client.Client, node *corev1.Node) (reconcile.Result, error) {
-	if err := client.Delete(ctx, node); err != nil {
-		return reconcile.Result{}, fmt.Errorf("Error deleting node: %v, error is: %v", node.Name, err)
-	}
-	return reconcile.Result{}, nil
-}
-
-func (r *providerIDReconciler) fetchOvirtVmID(nodeName string) (string, error) {
+func (r *providerIDController) fetchOvirtVmID(nodeName string) (string, error) {
 	c, err := r.GetConnection(common.NAMESPACE, common.CREDENTIALS_SECRET)
 	if err != nil {
 		return "", err
@@ -103,13 +74,13 @@ func (r *providerIDReconciler) fetchOvirtVmID(nodeName string) (string, error) {
 }
 
 func Add(mgr manager.Manager, opts manager.Options) error {
-	reconciler, err := NewProviderIDReconciler(mgr)
+	pic, err := NewProviderIDController(mgr)
 
 	if err != nil {
-		return fmt.Errorf("error building reconciler: %v", err)
+		return fmt.Errorf("error building ProviderIDController: %v", err)
 	}
 
-	c, err := controller.New("provdierID-controller", mgr, controller.Options{Reconciler: reconciler})
+	c, err := controller.New("ProviderIDController", mgr, controller.Options{Reconciler: pic})
 	if err != nil {
 		return err
 	}
@@ -123,14 +94,12 @@ func Add(mgr manager.Manager, opts manager.Options) error {
 	return nil
 }
 
-func NewProviderIDReconciler(mgr manager.Manager) (*providerIDReconciler, error) {
+func NewProviderIDController(mgr manager.Manager) (*providerIDController, error) {
 	log.SetLogger(klogr.New())
-	r := providerIDReconciler{
+	return &providerIDController{
 		BaseController: common.BaseController{
-			Log:    log.Log.WithName("controllers").WithName("providerID-reconciler"),
+			Log:    log.Log.WithName("controllers").WithName("providerIDController"),
 			Client: mgr.GetClient(),
 		},
-	}
-	r.fetchProviderIDFunc = r.fetchOvirtVmID
-	return &r, nil
+	}, nil
 }

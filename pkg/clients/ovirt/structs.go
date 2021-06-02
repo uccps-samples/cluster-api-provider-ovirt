b1bea0f647f92f4f6560b3dba4c57080275a3576
Gal-Zaidman <gzaidman@redhat.com>
2021-06-02 17:43:07 +0300

    move structs to structs file

--- pkg/clients/ovirt/structs.go
+++ pkg/clients/ovirt/structs.go
@@ -2,19 +2,19 @@ package ovirt
 
 import ovirtsdk "github.com/ovirt/go-ovirt"
 
+type ovirtClient struct {
+	connection *ovirtsdk.Connection
+}
+
 type Instance struct {
 	*ovirtsdk.Vm
 }
 
-type SshKeyPair struct {
-	Name string `json:"name"`
-
-	// PublicKey is the public key from this pair, in OpenSSH format.
-	// "ssh-rsa AAAAB3Nz..."
-	PublicKey string `json:"public_key"`
-
-	// PrivateKey is the private key from this pair, in PEM format.
-	// "-----BEGIN RSA PRIVATE KEY-----\nMIICXA..."
-	// It is only present if this KeyPair was just returned from a Create call.
-	PrivateKey string `json:"private_key"`
+type Creds struct {
+	URL      string
+	Username string
+	Password string
+	CAFile   string
+	Insecure bool
+	CABundle string
 }

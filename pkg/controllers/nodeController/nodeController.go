4561f8dc6cba92dddde6abb64e9e04e5f3c67dfb
Damiano Donati <damiano.donati@gmail.com>
2022-06-07 23:06:35 +0100

    bump deps to k8s-1.24

--- pkg/controllers/nodeController/nodeController.go
+++ pkg/controllers/nodeController/nodeController.go
@@ -11,7 +11,7 @@ import (
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/klog/klogr"
+	"k8s.io/klog/v2/klogr"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
